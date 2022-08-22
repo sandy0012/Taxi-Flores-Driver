13dc229729d24298abebbca1b89959b49f590050
AndreeFarro <andreefarro10@hotmail.com>
2022-08-21 19:49:04 -0500

    permisos de ubicacion

--- app/src/main/java/com/uns/taxifloresdriver/activities/MapActivity.kt
+++ app/src/main/java/com/uns/taxifloresdriver/activities/MapActivity.kt
@@ -1,17 +1,119 @@
 package com.uns.taxifloresdriver.activities
 
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import com.example.easywaylocation.EasyWayLocation
+import com.example.easywaylocation.Listener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.uns.taxifloresdriver.R
 import com.uns.taxifloresdriver.databinding.ActivityMapBinding
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
+
     private lateinit var binding : ActivityMapBinding
+    private var googleMap: GoogleMap? = null
+    private var easyWayLocation: EasyWayLocation? = null
+    private var myLocationLatLng: LatLng? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val locationRequest = LocationRequest.create().apply{
+            interval = 0
+            fastestInterval = 0
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+            smallestDisplacement = 1f
+        }
+
+        easyWayLocation = EasyWayLocation(this, locationRequest, false, false, this)
+
+        locationPermissions.launch(arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ))
+
+    }
+
+    val locationPermissions =registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            permission ->
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            when{
+                permission.getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION, false)->{
+                    Log.d("LOCALIZACION","Permiso Concedido")
+                    easyWayLocation?.startLocation();
+                }
+                permission.getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION, false)->{
+                    Log.d("LOCALIZACION","Permiso Concedido con Limitación")
+                    easyWayLocation?.startLocation();
+                }
+                else ->{
+                    Log.d("LOCALIZACION","Permiso no Concedido")
+                }
+            }
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        easyWayLocation?.startLocation();
+    }
+
+    override fun onDestroy() {//CUANDO CERRAMOS LA APP O PASAMOS A OTRA ACTIVYTI
+        super.onDestroy()
+        easyWayLocation?.endUpdates();
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
+        googleMap?.isMyLocationEnabled = true
+    }
+
+    override fun locationOn() {
+
+    }
+
+    override fun currentLocation(location: Location) {
+        myLocationLatLng = LatLng(location.latitude, location.longitude) //latitud y longitud de la posicion actual
+        googleMap?.moveCamera(
+            CameraUpdateFactory.newCameraPosition(
+            CameraPosition.builder().target(myLocationLatLng!!).zoom(17f).build()
+        ))
+    }
+
+    override fun locationCancelled() {
 
     }
 }
