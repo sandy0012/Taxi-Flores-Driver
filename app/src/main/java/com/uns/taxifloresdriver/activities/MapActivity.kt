4b5a917fbbc7d679038a56d0fdac364be785726d
AndreeFarro <andreefarro10@hotmail.com>
2022-08-21 21:00:02 -0500

    cambio de icono - carro

--- app/src/main/java/com/uns/taxifloresdriver/activities/MapActivity.kt
+++ app/src/main/java/com/uns/taxifloresdriver/activities/MapActivity.kt
@@ -1,6 +1,10 @@
 package com.uns.taxifloresdriver.activities
 
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.location.Location
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +13,7 @@ import android.util.Log
 import android.view.WindowManager
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.easywaylocation.EasyWayLocation
 import com.example.easywaylocation.Listener
 import com.google.android.gms.location.LocationRequest
@@ -17,8 +22,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.*
 import com.uns.taxifloresdriver.R
 import com.uns.taxifloresdriver.databinding.ActivityMapBinding
 
@@ -28,6 +32,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
     private var googleMap: GoogleMap? = null
     private var easyWayLocation: EasyWayLocation? = null
     private var myLocationLatLng: LatLng? = null
+    private var markerDriver: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,9 +80,42 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
     }
 
 
+    private fun addMarker(){
+        val drawable = ContextCompat.getDrawable(applicationContext,R.drawable.car)
+        val markerIcon = getMarkerFromDrawable(drawable!!)
+        if(markerDriver != null){
+            markerDriver?.remove()
+        }
+        markerDriver = googleMap?.addMarker(
+            MarkerOptions()
+                .position(myLocationLatLng!!)
+                .anchor(0.5f,0.5f)
+                .flat(true)
+                .icon(markerIcon)
+        )
+
+    }
+
+    //insercion de drawable
+    private fun getMarkerFromDrawable(drawable: Drawable): BitmapDescriptor{
+        val canvas = Canvas()
+        val width = 65
+        val heigh = 100
+
+        val bitmap = Bitmap.createBitmap(
+            width,
+            heigh,
+            Bitmap.Config.ARGB_8888
+        )
+        canvas.setBitmap(bitmap)
+        drawable.setBounds(0,0,width,heigh)
+        drawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+    //ejecuta cada ves que se abre la aplicacio
     override fun onResume() {
         super.onResume()
-        easyWayLocation?.startLocation();
     }
 
     override fun onDestroy() {//CUANDO CERRAMOS LA APP O PASAMOS A OTRA ACTIVYTI
@@ -87,6 +125,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
 
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
+        easyWayLocation?.startLocation();
+
         if (ActivityCompat.checkSelfPermission(
                 this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION
@@ -98,19 +138,25 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
 
             return
         }
-        googleMap?.isMyLocationEnabled = true
+        googleMap?.isMyLocationEnabled = false
     }
 
     override fun locationOn() {
 
     }
 
+    /**
+     * Actualizacion de la ubicacion en tiempo real
+     */
     override fun currentLocation(location: Location) {
         myLocationLatLng = LatLng(location.latitude, location.longitude) //latitud y longitud de la posicion actual
+
         googleMap?.moveCamera(
             CameraUpdateFactory.newCameraPosition(
             CameraPosition.builder().target(myLocationLatLng!!).zoom(17f).build()
         ))
+
+        addMarker()
     }
 
     override fun locationCancelled() {
